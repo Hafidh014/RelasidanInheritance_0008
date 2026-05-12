30bd071f250e5702518375447c23db22a3666cda
muhammad.hafidh2006@gmail.com <muhammad.hafidh2006@gmail.com>
2026-05-12 17:43:43 +0700

    deklarasi dam pemberian nilai variable

--- agregasi.cpp
+++ agregasi.cpp
@@ -6,3 +6,13 @@ using namespace std;
 #include "anak.h"
 #include "ibu.h"
 
+int main()
+{
+    Ibu* varIbu = new Ibu("rani");
+    Ibu* varIbu2 = new Ibu("sari");
+    anak* varAnak1 = new anak("tono");
+    anak* varAnak2 = new anak("tiara");
+    anak* varAnak3 = new anak("dini");
+
+
+}
